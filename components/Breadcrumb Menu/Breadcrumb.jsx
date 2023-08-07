7ad7d860f58b3437d1d92655b8fb5a3326c7efd7
unknown <dunkwualex6@gmail.com>
2023-08-07 09:27:12 +0100

    fix breadcrumb

--- components/Breadcrumb Menu/Breadcrumb.jsx
+++ components/Breadcrumb Menu/Breadcrumb.jsx
@@ -25,12 +25,16 @@ const Breadcrumb = ({items = [
 <nav class="relative md:m-5 flex flex-wrap items-center justify-between py-3 bg-ry-200 text-pry-800 hover:text-pry-800 focus:text-pry-800 navbar navbar-expand-lg navbar-light">
   <div class="container-fluid w-full flex flex-wrap items-center justify-between px-6">
     <nav class="bg-grey-light rounded-md w-full" aria-label="breadcrumb">
-      <ol class="list-reset flex">
+      <ol class="list-reset flex flex-wrap">
         {
             items.map(({title, href}, index) => (
                 <div key={index} className="flex link-reset">
                     <li><Link href={href} class="text-pry-800 capitalize text-xs md:text-sm font-lato hover:text-pry-800">{title}</Link></li>
-                    <li><span class="text-pry-800 text-xs md:text-sm mx-1 md:mx-5">/</span></li>
+                    {
+                      items.length !== index+1 && (
+                        <li><span class="text-pry-800 text-xs md:text-sm mx-1 md:mx-5">/</span></li>
+                      )
+                    }                    
                 </div> 
             ))
         }
